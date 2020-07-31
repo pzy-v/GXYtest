a394e3efd984df89f301188dcdf9e6e294cffd7b
peter <25470@qq.com>
2020-07-31 15:43:53 +0800

    number 1.6

--- Test0731/src/内苏奥01/Test.java
+++ Test0731/src/内苏奥01/Test.java
@@ -4,9 +4,8 @@ public class Test {
 	public static void main(String[] args) {
 	System.out.println("HelloWorld");
 	int a=10;
-	System.out.println(a);
-	int b=20;
-	System.out.println(b);	
+	int c=20;	
+	
 	}
 
 }

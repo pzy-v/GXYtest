f65323d150178a17f3aa3242d36092265886a5d5
pzy-v <69029785+pzy-v@users.noreply.github.com>
2020-07-31 15:06:57 +0800

    Update and rename Test0731/src/nesuo01/Test.java to Test0731/SRC/内苏奥01/Test.java
    number v1.3

--- Test0731/SRC/内苏奥01/Test.java
+++ Test0731/SRC/内苏奥01/Test.java
@@ -5,6 +5,10 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("HelloWorld");
+		int a=10;
+		System.out.println(a);
+		int b=20;
+		System.out.println(b);	
 	}
 
 }

f8bb00f1720de86e291b659eaa95271569c299ac
peter <25470@qq.com>
2020-07-31 16:06:41 +0800

    number v1.8

--- Test0731/src/woaini01/Test.java
+++ Test0731/src/woaini01/Test.java
@@ -4,9 +4,9 @@ public class Test {
 	public static void main(String[] args) {
 	System.out.println("HelloWorld");
 	int a=10;
-		System.out.println(a)
+		System.out.println(a);
 	int c=20;
-		System.out.println(c)
+		System.out.println(c);
 	
 	}
 
